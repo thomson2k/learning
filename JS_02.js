e2e152af6f9a58670cf3252f57dbd18ea48e96e3
wirycapybara <w.t.tomasz@gmail.com>
2018-01-20 22:49:38 +0100

    Conditions statements

---
+++ JS_02.js
@@ -0,0 +1,18 @@
+// Conditions statments
+
+//Using conditions,
+//check which variable is the largest
+  var x = 10;
+  var y = 60;
+  var z = 2;
+
+
+  if (x > y && x > z) {
+      console.log("x is the largest");
+  } else {
+      if (b > c) {
+          console.log("y is the largest");
+      } else {
+          console.log("z is the largest");
+      }
+  }
